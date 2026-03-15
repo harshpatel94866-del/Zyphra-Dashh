65176fbe8da84bd9c8c6914e08cf43bf15615cd6
harshpatel94866-del <harshpatel94866@gmail.com>
2026-03-15 14:22:00 +0530

    Update api.ts

--- src/api.ts
+++ src/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { secureStorage } from './utils/secureStorage';
 
 const api = axios.create({
-  baseURL: 'http://172.174.246.240:25255/api',
+  baseURL: process.env.NODE_ENV === 'development' ? 'http://172.174.246.240:25255/api' : '/api',
 });
 
 api.interceptors.request.use((config) => {
